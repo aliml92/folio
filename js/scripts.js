3dab776229d8ac22f3f95e5b1fcc8dd118df00d8
aliml92 <almowins5yearsss@gmail.com>
2021-08-16 22:47:53 +0900

    i18n added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,217 @@
+let tr = [
+    {
+        eng: 'I am a',
+        kor: '저는',
+    },
+    {
+        eng: 'Backend Engineer',
+        kor: '백엔드 엔지니어',
+    },
+    {
+        eng: 'Discover',
+        kor: '반견하기',
+    },
+    {
+        eng: 'Contact Me',
+        kor: '연락',
+    },
+    {
+        eng: "Hi I'm Alisher Muzaffarov,",
+        kor: '안녕하세요 Alisher Muzaffarov입니다.',
+    },
+    {
+        eng: 'And I love to create challenging projects that requires me to use new technologies. I love going through the entire process from concept, to design and then development and launch.',
+        kor: '저는 새로운 기술을 사용해야 하는 도전적인 프로젝트를 만드는 것을 좋아합니다. 컨셉부터 디자인, 개발, 런칭까지 모든 것을 살펴보는 것을 좋아합니다.',
+    },
+    {
+        eng: 'Education',
+        kor: '학력',
+    },
+    {
+        eng: "Bachelor's in Physics",
+        kor: '물리학 학사',
+    },
+    {
+        eng: "University of Ulsan",
+        kor: '울산대학교',
+    },
+    {
+        eng: "Master's in IT",
+        kor: 'IT 분야의 석사',
+    },
+    {
+        eng: 'Offered Services',
+        kor: '서비스',
+    },
+    {
+        eng: 'Front-end Integration',
+        kor: '프런트-엔드 통합',
+    },
+    {
+        eng: 'Integrating the backend with front-end using server-side rendering templates. Also able to work with SPA applications like React.js',
+        kor: '서버측 렌더링 템플릿을 사용하여 백엔드와 프런트엔드를 통합합니다. React.js와 같은 SPA 애플리케이션에서도 작업할 수 있습니다.',
+    },
+    {
+        eng: 'Data Storage Solutions',
+        kor: '데이터 스토리지 솔루션',
+    },
+    {
+        eng: 'Designing optimal data models for both SQL and NoSQL databases from scratch. Also implementing them with efficient query resolutions.',
+        kor: 'SQL 데이터베이스와 NoSQL 데이터베이스 모두에 대해 처음부터 최적의 데이터 모델을 설계합니다. 또한 효율적인 쿼리 해결 방법으로 이를 구현합니다.',
+    },
+    {
+        eng: "Quick Problem Solving",
+        kor: "빠른 문제해결.",
+    },
+    {
+        eng: "Finding bugs and issues and resolving them by applying different resources, tools and third party libraries",
+        kor: "다양한 리소스, 도구 및 타사 라이브러리를 적용하여 버그와 문제를 찾아 해결합니다.",
+    },
+    {
+        eng: "Skills",
+        kor: "기술",
+    },
+    {
+        eng: "DEVELOPMENT TOOLS & TECHS",
+        kor: "개발 툴 & 기술",
+    },
+    {
+        eng: "I use the following tools and services on a daily basis",
+        kor: "저는 매일 다음과 같은 툴와 서비스를 사용합니다",
+    },
+    {
+        eng: "DEVELOPMENT SKILLS",
+        kor: "개발 기술",
+    },
+    {
+        eng: "I am familiar and work with the following languages and libraries",
+        kor: "저는 다음과 같은 프로그래밍 언어와 라이브러리를 사용합니다.",
+    },
+    {
+        eng: "Projects",
+        kor: "프로젝트",
+    },
+    {
+        eng: "These are my personal projects that I did for improving my practical coding skills.They are deployed on three different platforms: Digitalocean, Amazon EC2 and Netlify.",
+        kor: "이것들은 저의 실무적 코딩 능력 향상을 위해 제 개인 프로젝트입니다.Digital Ocean, Amazon EC2 및 Netlify의 세 가지 플랫폼에 구축되어 있습니다.",
+    },
+    {
+        eng: "Blog Website",
+        kor: "블로그 웹사이트",
+    },
+    {
+        eng: "This blog website, built on spring stack, has an index based search functionality and admin panel.",
+        kor: "Spring 스택에 구축된 이 블로그 웹 사이트에는 인덱스 기반 검색 기능과 관리 패널이 있습니다",
+    },
+    {
+        eng: "Visit Site",
+        kor: "사이트 방문",
+    },
+    {
+        eng: "Source",
+        kor: "소스",
+    },
+    {
+        eng: "eCommerce",
+        kor: "eCommerce",
+    },
+    {
+        eng: "This eCommerce project, being deployed on EC2 instance on AWS, is based on Spring Boot, MongoDB and React.js.",
+        kor: "이 eCommerce 프로젝트는 Spring Boot, MongoDB 및 React.js를 기반으로 합니다.",
+    },
+    {
+        eng: "Visit Site",
+        kor: "사이트 방문",
+    },
+    {
+        eng: "Source",
+        kor: "소스",
+    },
+    {
+        eng: "Web Scraper",
+        kor: "웹 스크래퍼",
+    },
+    {
+        eng: "This js application scrapes Amazon's product and search pages to extract data on a product, namely laptops.",
+        kor: "이 js 애플리케이션은 Amazon의 제품 및 검색 페이지를 스크래핑하여 제품(노트북)에서 데이터를 추출합니다.",
+    },
+    {
+        eng: "Visit Site",
+        kor: "사이트 방문",
+    },
+    {
+        eng: "Source",
+        kor: "소스",
+    },
+    {
+        eng: "Video Chat",
+        kor: "비디오 채팅 앱",
+    },
+    {
+        eng: "This application uses WebRTC technology to create a peer-to-peer connection between two different browsers.Firestore database takes care of signaling process.",
+        kor: "이 웹 앱은 WebRTC 기술을 사용하여 서로 다른 두 브라우저 간에 피어 투 피어 연결을 만듭니다.Firestore 데이터베이스는 신호 전달 프로세스를 관리합니다.",
+    },
+    {
+        eng: "Visit Site",
+        kor: "사이트 방문",
+    },
+    {
+        eng: "Source",
+        kor: "소스",
+    },
+    {
+        eng: "Contact",
+        kor: "연락",
+    },
+    {
+        eng: "For any type of inquiry, please don't hesitate to get in touch with me. The fastest way is to send me your message using the following email",
+        kor: "어떤 문의라도 주저하지 말고 연락 주세요. 가장 빠른 방법은 다음 메일로 메시지를 저에게 보내는 것입니다.",
+    },
+    {
+        eng: "Name",
+        kor: "성명",
+    },
+    {
+        eng: "Email",
+        kor: "이메일",
+    },
+    {
+        eng: "Message",
+        kor: "메시지",
+    },
+    {
+        eng: "Submit",
+        kor: "제출하기",
+    },
+]
+
+
+let eng = document.getElementById('eng');
+let kor = document.getElementById('kor');
+let i18n = document.getElementsByClassName('i18n');
+
+eng.addEventListener('click', (event) => {
+    let langUI = document.getElementById('lang');
+    let lang = langUI.getAttribute('data-lang');
+    event.preventDefault();
+    if (lang != 'eng') {
+        for (let i = 0; i < i18n.length; i++) {
+            i18n[i].innerText = tr[i].eng;
+        }
+        langUI.setAttribute('data-lang', 'eng');
+    }
+});
+kor.addEventListener('click', (event) => {
+    let langUI = document.getElementById('lang');
+    let lang = langUI.getAttribute('data-lang');
+    console.log(event.target);
+    if (lang != 'kor') {
+        for (let i = 0; i < i18n.length; i++) {
+            i18n[i].innerText = tr[i].kor;
+        }
+        langUI.setAttribute('data-lang', 'kor');
+    }
+});
 
 (function($) {
     "use strict"; 
